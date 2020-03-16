a5db52de455bb5aaf40e8e063ceab1e6203bd764
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2020-03-16 17:06:26 +0100

    Initial project creation.

---
+++ swap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swap
+//
+//  Created by Maximilian Hvesser-Lewis on 16/03/2020.
+//  Copyright © 2020 Hvesser. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
