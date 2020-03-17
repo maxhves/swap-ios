4bcdc7491e52ee8d0d8915e83bf0119093c8978b
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2020-03-17 08:58:07 +0100

    Added GridStack view for use with keypad.

---
+++ swap/UI/Views/Helper/GridStack.swift
@@ -0,0 +1,21 @@
+//
+//  GridStack.swift
+//  swap
+//
+//  Created by Maximilian Hvesser-Lewis on 17/03/2020.
+//  Copyright © 2020 Hvesser. All rights reserved.
+//
+
+import SwiftUI
+
+struct GridStack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GridStack_Previews: PreviewProvider {
+    static var previews: some View {
+        GridStack()
+    }
+}
---
+++ swap/UI/Views/Keypad/Keypad.swift
@@ -0,0 +1,21 @@
+//
+//  Keypad.swift
+//  swap
+//
+//  Created by Maximilian Hvesser-Lewis on 16/03/2020.
+//  Copyright © 2020 Hvesser. All rights reserved.
+//
+
+import SwiftUI
+
+struct Keypad: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Keypad_Previews: PreviewProvider {
+    static var previews: some View {
+        Keypad()
+    }
+}
