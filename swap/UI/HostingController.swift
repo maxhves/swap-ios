8a06fa467659348050a3fc470e015721c2b29ffb
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2020-03-17 14:58:10 +0100

    Added a HostingController class to achieve a constant light status bar.

---
+++ swap/UI/HostingController.swift
@@ -0,0 +1,18 @@
+//
+//  HostingController.swift
+//  swap
+//
+//  Created by Maximilian Hvesser-Lewis on 17/03/2020.
+//  Copyright © 2020 Hvesser. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+class HostingController: UIHostingController<ContentView> {
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+}
