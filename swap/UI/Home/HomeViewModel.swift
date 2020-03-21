371bbed618c79018c4145e17c47c661aa0d55244
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2020-03-21 17:41:21 +0100

    Added HomeViewModel class with small implementation of exchange rates API.

---
+++ swap/UI/Home/HomeViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  HomeViewModel.swift
+//  swap
+//
+//  Created by Maximilian Hvesser-Lewis on 21/03/2020.
+//  Copyright © 2020 Hvesser. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+class HomeViewModel: NetworkViewModel, ObservableObject {
+    
+    typealias NetworkResource = String
+    
+    var resource: Resource<NetworkResource> = .loading
+    var network: Network
+    var route: NetworkRoute = ExchangeRatesRoute.rate
+    var bag: Set<AnyCancellable> = Set<AnyCancellable>()
+    
+    init(with network: Network) {
+        self.network = network
+    }
+    
+    func onAppear() {}
+    
+}
